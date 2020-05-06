a33660ba9ca9638d89670bb4d4f89cda858d5242
sgraton <steven.graton@gmail.com>
2020-05-06 12:38:50 +0200

    Authentification for logout

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :authenticate_with_token, only: [:logout]
+    before_action :authenticate_with_token!, only: [:logout]
 
     def facebook
         if params[:facebook_access_token]
